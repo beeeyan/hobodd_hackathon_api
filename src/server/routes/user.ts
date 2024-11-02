7ffcabb8aac4c322e1fcd628a70caf36fddd9a1c
izu12ki <hirano.i@docodoor.co.jp>
2024-11-03 01:12:51 +0900

    カレンダーデータ取得API
    https://scrapbox.io/flutter-hackathon-2024/API

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -1,8 +1,8 @@
 import { zValidator } from "@hono/zod-validator";
-import { eq } from "drizzle-orm";
+import { asc, desc, eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { z } from "zod";
-import { room, users } from "../../../drizzle/schema";
+import { logs, room, users } from "../../../drizzle/schema";
 import type { Bindings } from "../../middleware/db";
 import { generateULIDLike } from '../utils/generateUniqueID';
 const app = new Hono<{ Bindings: Bindings }>();
@@ -82,6 +82,48 @@ export const route = app
 				return c.json({
 					success: false,
 					error: 'Failed to create user'
+				}, 500);
+			}
+		},
+	)
+	.get(
+		"/:room_id",
+		zValidator(
+			"param",
+			z.object({
+				room_id: z.string(),
+			}),
+		),
+		async (c) => {
+			const { room_id } = c.req.valid("param");
+
+			try {
+				const latest_user_logs_of_the_room = await c.var.db
+					.select({
+						username: users.username,
+						createdAt: users.createdAt,
+						sticker: logs.sticker,
+						clickedAt: logs.clickedAt,
+					})
+					.from(users)
+					.leftJoin(logs, eq(users.id, logs.userId))
+					.where(eq(users.roomId, room_id))
+					.groupBy(users.id)
+					.orderBy(
+						asc(users.createdAt),
+						desc(logs.clickedAt)
+					);
+
+				console.log({ latest_user_logs_of_the_room })
+
+				return c.json(latest_user_logs_of_the_room);
+
+			} catch (error) {
+				// エラーハンドリング
+				console.error('Database error:', error);
+				return c.json({
+					success: false,
+					error: 'Failed to find logs for the room'
 				}, 500);
 			}
 		},
--- src/server/utils/dateTimeConverter.ts
+++ src/server/utils/dateTimeConverter.ts
@@ -1,1 +1,8 @@
+// 現在日時を文字列で返す
 export const getCurrentTimestamp = () => new Date().toISOString();
+
+// 現在日付をyyyy-mm-ddで返す
+export const getFormattedDate = (): string => {
+  const date = new Date();
+  return date.toISOString().split('T')[0];
+};
