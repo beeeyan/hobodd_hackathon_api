3df178bbb64d00db3e12c321bb47d5efbb29ae16
izu12ki <121919242+izu12ki@users.noreply.github.com>
2024-11-02 23:46:22 +0900

    ルームメンバーの最新情報を返すAPI (#4)
    https://scrapbox.io/flutter-hackathon-2024/API

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,6 +4,7 @@ import { db } from "../middleware/db.js";
 import { route as greetingRoute } from "./routes/greeting.js";
 import { route as helloRoute } from "./routes/hello.js";
 import { route as logRoute } from "./routes/log.js";
+import { route as roomRoute } from "./routes/room.js";
 import { route as userRoute } from "./routes/user.js";
 
 type Input = {
@@ -23,6 +24,7 @@ export function createApp({ basePath }: Input) {
 		.route("/hello", helloRoute)
 		.route("/greeting", greetingRoute)
 		.route("/user", userRoute)
+		.route("/room", roomRoute)
 		.route("/log", logRoute);
 
 	return { app, route };
