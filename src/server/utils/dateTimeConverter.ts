efeac1a260b6a1084cf9bd223a447ba43e0abaf3
izu12ki <hirano.i@docodoor.co.jp>
2024-11-03 12:32:08 +0900

    カレンダーAPIが今日の日付文のデータも返すようにする

--- src/server/utils/dateTimeConverter.ts
+++ src/server/utils/dateTimeConverter.ts
@@ -2,9 +2,11 @@
 export const getCurrentTimestamp = () => new Date().toISOString();
 
 // 現在日付をyyyy-mm-ddで返す
-export const getFormattedDate = (): string => {
+export const getFormattedDate = (addDays: number = 0): string => {
   const date = new Date();
   // 日本時間に変換
   const jstDate = new Date(date.getTime() + (9 * 60 * 60 * 1000)); // UTC+9時間
+  // 指定された日数を加算
+  jstDate.setDate(jstDate.getDate() + addDays);
   return jstDate.toISOString().split('T')[0];
 };
